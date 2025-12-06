1d2186d219e95ae091a85d25eb3cd412ad706320
emilfmi.lang <emilfmi@code.edu.az>
2025-12-06 13:16:28 +0400

    first commit

---
+++ Task(12.03.2025)/Task(12.03.2025)/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Task_12._03._2025_.Controllers;
+public class HomeController : Controller
+{
+    public JsonResult Index()
+    {
+        return Json(new { message = "Hello from HomeController!" });
+    }
+}
---
+++ Task(12.03.2025)/Task(12.03.2025)/Models/Car.cs
@@ -0,0 +1,5 @@
+﻿namespace Task_12._03._2025_.Models;
+
+public class Car
+{
+}
